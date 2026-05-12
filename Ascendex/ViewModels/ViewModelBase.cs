5461bb99b891ed085eab7c5c0336e701be15b635
Sylvain <slorange7@gmail.com>
2026-05-11 22:43:25 -0400

    Init Avalonia project

---
+++ Ascendex/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ascendex.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Ascendex/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Ascendex.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
